6408aa2b695a66e67b12698c78675ebf496a0bb7
vikki <vikki@gmail.com>
2020-07-02 19:33:17 +0530

    spa

---
+++ app/route.js
@@ -0,0 +1,20 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.controller("myCtrl", function($scope){
+     $scope.myShow = false;
+  $scope.myClick = function() {
+    $scope.myShow =!$scope.myShow ;}
+})
+app.config(function ($routeProvider, $locationProvider) { 
+        $locationProvider.hashPrefix(''); 
+        $routeProvider
+            .when("/home", {
+                templateUrl: "template/home.html"
+            })
+            .when("/about", {
+                templateUrl: "template/about.html"
+            })
+            .when("/contact", {
+                templateUrl: "template/contact.html"
+            })
+    });
